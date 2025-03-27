df9aa787bfcc53a63801f3d84748eed52972ee98
BrunoFgR <brunofds281@gmail.com>
2025-03-26 21:23:16 -0300

    create function to convert from markdown to html

--- src/helpers.py
+++ src/helpers.py
@@ -17,3 +17,23 @@ def text_node_to_html_node(text_node: TextNode):
             return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"Unknown text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_node = []
+        section = old_node.text.split(delimiter)
+        if len(section) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(section)):
+            if section[i] == "":
+                continue
+            if i % 2 == 0:
+                split_node.append(TextNode(section[i], TextType.TEXT))
+            else:
+                split_node.append(TextNode(section[i], text_type))
+        new_nodes.extend(split_node)
+    return new_nodes
